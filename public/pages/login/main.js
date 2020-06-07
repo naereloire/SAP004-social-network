0ea2ac2d8d526c9aebac8da46aafc250ca47a2d2
naereloire <naere.loire@gmail.com>
2020-06-06 21:42:08 -0300

    ajustes de estilo e posicionamentos

--- public/pages/login/main.js
+++ public/pages/login/main.js
@@ -1,25 +1,22 @@
-export default() => {
+export default () => {
   const container = document.createElement('div');
 
   container.innerHTML = `
-      <div class="container-login">
-
-    <section class='login-img'>
-    <img class="logo-img"src="./img/girly-logo.png" alt="imagem do logo">
-  </section>
-  
-          <h1 class='title'>[girlyHub]</h1>
-          <h3 class='subtitle'>Bem vinda!</h3>
-          <form class="login-form">
-          <input id='email' type='email' class= 'email-input', placeholder= '  Email'>
-          <input id= 'password' type='password' class= 'password-input' placeholder= '  Senha'>
-          <button id='login-btn' class=' login-btn' >Log in</button><br/>
-          </form>
-          <hr>
-          <p>Ainda não tem uma conta? <link rel="" href=""> Crie agora </p></>
-          <hr>
-          <div id="firebaseui-auth"></div>
-        <div>
+    <div class="container-login">
+      <img class="logo-img" src="./img/girly-logo.png" alt="imagem do logo">
+      <section >
+      <h1 class='title'>[girlyHub]</h1>
+      <h3 class='subtitle'>Bem vinda!</h3>
+     <form class="login-form">
+      <input id='email' type='email' class='email-input' , placeholder='  Email'>
+      <input id='password' type='password' class='password-input' placeholder='  Senha'>
+      <button id='login-btn' class=' login-btn'>Log in</button><br />
+    </form>
+    <hr>
+    <p>Ainda não tem uma conta? <link rel="" href=""> Crie agora </p>
+    <div id="firebaseui-auth"></div>
+    </section>
+    <div>
         `;
 
   return container;
