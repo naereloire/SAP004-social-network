b0aa5dfde9eaf8e5ca5c226788b72b08a6b8bd32
Evellyn <xe.uyemura@gmail.com>
2020-06-10 11:46:45 -0300

    btn voltar

--- public/index.js
+++ public/index.js
@@ -13,8 +13,28 @@ const init = () => window.addEventListener("hashchange", renderPage)
 
 const renderPage = () => {
     main.innerHTML = " ";
+    authentication()
+}
+
+window.addEventListener("hashchange", renderPage)
+const validateHash = (hash) => hash === "" ? "home" : hash.replace("#", "")
+
+window.addEventListener("load", () => {
+    renderPage();
+    init();
+})
+
+const sidebarAction = (event) => {
+    event.currentTarget.id === "open-sidebar" ?
+        document.getElementById("side-navigation").style.width = "250px" :
+        document.getElementById("side-navigation").style.width = "0";
+}
+
+document.getElementById("close-sidebar").addEventListener("click", sidebarAction)
+document.getElementById("open-sidebar").addEventListener("click", sidebarAction)
+
+function authentication () {
     let page = validateHash(window.location.hash)
-    //Busca se existe um usuário logado ou deslogado.
     firebase.auth().onAuthStateChanged((usuario) => {
         if(!usuario){
             if(page === "register") {
@@ -23,6 +43,11 @@ const renderPage = () => {
                 btnRegister.addEventListener("click", function (event) {
                     event.preventDefault()
                     auth.createLogin()
+                })
+                const backBtn = document.querySelector("#back-btn");
+                backBtn.addEventListener("click", function(event) {
+                    event.preventDefault()
+                    window.location.href ="/#login"
                 })
             }
             else {
@@ -45,20 +70,3 @@ const renderPage = () => {
         }
     })
 }
-
-window.addEventListener("hashchange", renderPage)
-const validateHash = (hash) => hash === "" ? "home" : hash.replace("#", "")
-
-window.addEventListener("load", () => {
-    renderPage();
-    init();
-})
-
-const sidebarAction = (event) => {
-    event.currentTarget.id === "open-sidebar" ?
-        document.getElementById("side-navigation").style.width = "250px" :
-        document.getElementById("side-navigation").style.width = "0";
-}
-
-document.getElementById("close-sidebar").addEventListener("click", sidebarAction)
-document.getElementById("open-sidebar").addEventListener("click", sidebarAction)
--- public/pages/registro/main.js
+++ public/pages/registro/main.js
@@ -4,13 +4,16 @@ export default () => {
     const template = `
         <h1 class="text-profile">Preencha os campos abaixo:</h1>
         <form class="form-profile">
-            <input class="register" type="text" placeholder="Nome *" required>
-            <input class="register" type="text" placeholder="Sobrenome *" required>
-            <input class="register" type="date" placeholder="Data de nascimento *" required>
-            <input id="email" class="register" type="email" placeholder="Email *" required>
-            <input id="password" class="register" type="password" placeholder="Senha *" required>
-            <input class="register" type="text" placeholder="Cidade *" required>
-            <input id="btn-register" class="register" type="submit">
+            <input type="text" placeholder="Nome *" required>
+            <input type="text" placeholder="Sobrenome *" required>
+            <input type="date" placeholder="Data de nascimento *" required>
+            <input id="email" type="email" placeholder="Email *" required>
+            <input id="password" type="password" placeholder="Senha *" required>
+            <input type="text" placeholder="Cidade *" required>
+            <div id="btn-display">
+                <input id="back-btn" type="button" value="Voltar">
+                <input id="btn-register" type="submit">
+            </div>
         </form>
     `
     container.innerHTML = template;
