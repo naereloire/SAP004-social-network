895c7650811bf5de2df2a16814b1a9dd3c19d10d
Evellyn <xe.uyemura@gmail.com>
2020-06-11 17:27:20 -0300

    modificacoes

--- public/routes.js
+++ public/routes.js
@@ -1,21 +1,11 @@
 import home from './pages/home/main.js';
 import login from './pages/login/main.js';
-import geek from './pages/geek/main.js';
-import tech from './pages/tech/main.js';
-import autocuidado from './pages/autocuidado/main.js';
-import seguranca from './pages/seguranca/main.js';
-import oportunidades from './pages/oportunidades/main.js';
 import register from "./pages/registro/main.js"
 import profile from "./pages/perfil/main.js"
 
 export default {
-    home:home(),
+    home: home(),
     login: login(),
-    geek:geek(),
-    tech:tech(),
-    autocuidado:autocuidado(),
-    seguranca:seguranca(),
-    oportunidades:oportunidades(),
     register: register(),
     profile: profile()
 }
