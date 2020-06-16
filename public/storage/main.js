05faf6708f58b73768c9713d63b69b3742edc057
Evellyn <xe.uyemura@gmail.com>
2020-06-16 18:02:59 -0300

    storage

--- public/storage/main.js
+++ public/storage/main.js
@@ -1,15 +1,15 @@
+let ref = firebase.storage().ref('users')
+
 export const picture = () => {
-    const profilePicture = document.getElementById("profile-picture");
-    profilePicture.addEventListener("click", function(){
-    let ref = firebase.storage().ref("pictures");
-    let upload;
+    const userPicture = document.getElementById("profile-picture");
+    userPicture.addEventListener("click", function(){
+        userPicture.onchange = function (event) {
+            let archive = event.target.files[0]
 
-    profilePicture.onchange = function(event){
-        console.log("Oi")
-        let archive = event.target.files[0];
-        let uid = firebase.firestore().ref().push().key
-        upload = ref.child(uid).put(archive)
-    }
-})
-} 
-
+            ref.child('picture').put(archive)
+            .then(snapshot => {
+                console.log(snapshot)
+            })
+        }
+    })
+}
