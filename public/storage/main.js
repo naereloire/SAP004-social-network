55932533efdde15a05ae8d645903fc480bb60523
Evellyn <xe.uyemura@gmail.com>
2020-06-22 15:10:24 -0300

    commitando

--- public/storage/main.js
+++ public/storage/main.js
@@ -1,73 +1,71 @@
 export const openImageProfile = () => {
-  const userPicture = document.getElementById("profile-picture");
-  userPicture.addEventListener("click", function(){
-      let input = document.getElementById('file-input')
-      input.click()
-  })
+    const userPicture = document.getElementById("profile-picture");
+    userPicture.addEventListener("click", function () {
+        let input = document.getElementById('file-input')
+        input.click()
+    })
 }
 
 export const addImageProfile = () => {
-  let input = document.getElementById('file-input')
-  input.addEventListener('change', function(event){
-      let archive = event.target.files[0]
-      firebase.auth().onAuthStateChanged((user) =>{
-          let ref = firebase.storage().ref('users/profilePicture')
-          ref.child(user.uid).put(archive)
-          .then(snapshot => {
-              ref.child(user.uid).getDownloadURL().then(url => {
-                  let img = document.getElementById("image-profile");
-                  img.src = url
-              });
-          })
-      })
-  })
+    let input = document.getElementById('file-input')
+    input.addEventListener('change', function (event) {
+        let archive = event.target.files[0]
+        firebase.auth().onAuthStateChanged((user) => {
+            let ref = firebase.storage().ref('users/profilePicture')
+            ref.child(user.uid).put(archive).then(snapshot => {
+                ref.child(user.uid).getDownloadURL().then(url => {
+                    let img = document.getElementById("image-profile");
+                    img.src = url
+                });
+            })
+        })
+    })
 }
 
 export const putImageProfile = () => {
-  firebase.auth().onAuthStateChanged((user) =>{
-      let ref = firebase.storage().ref('users/profilePicture')
-      ref.child(user.uid).getDownloadURL().then(url => {
-          if(url) {
-              let photo = document.getElementById("image-profile");
-              photo.src = url
-          }
-      });
-  })
+    firebase.auth().onAuthStateChanged((user) => {
+        let ref = firebase.storage().ref('users/profilePicture')
+        ref.child(user.uid).getDownloadURL().then(url => {
+            if (url) {
+                let photo = document.getElementById("image-profile");
+                photo.src = url
+            }
+        });
+    })
 }
 
 export const openImageCover = () => {
-  const userCoverPicture = document.getElementById("cover-picture");
-  userCoverPicture.addEventListener("click", function(){
-      let coverInput = document.getElementById('file-cover-input')
-      coverInput.click()
-  })
+    const userCoverPicture = document.getElementById("cover-picture");
+    userCoverPicture.addEventListener("click", function () {
+        let coverInput = document.getElementById('file-cover-input')
+        coverInput.click()
+    })
 }
 
 export const addCoverImage = () => {
-  let coverInput = document.getElementById('file-cover-input')
-  coverInput.addEventListener('change', function(event){
-      let archive = event.target.files[0]
-      firebase.auth().onAuthStateChanged((user) =>{
-          let ref = firebase.storage().ref('users/coverPicture')
-          ref.child(user.uid).put(archive)
-          .then(snapshot => {
-              ref.child(user.uid).getDownloadURL().then(url => {
-                  let image = document.getElementById("cover-image");
-                  image.src = url
-              });
-          })
-      })
-  })
+    let coverInput = document.getElementById('file-cover-input')
+    coverInput.addEventListener('change', function (event) {
+        let archive = event.target.files[0]
+        firebase.auth().onAuthStateChanged((user) => {
+            let ref = firebase.storage().ref('users/coverPicture')
+            ref.child(user.uid).put(archive).then(snapshot => {
+                ref.child(user.uid).getDownloadURL().then(url => {
+                    let image = document.getElementById("cover-image");
+                    image.src = url
+                });
+            })
+        })
+    })
 }
 
 export const putCoverImage = () => {
-  firebase.auth().onAuthStateChanged((user) =>{
-      let ref = firebase.storage().ref('users/coverPicture')
-      ref.child(user.uid).getDownloadURL().then(url => {
-          if(url) {
-              let image = document.getElementById("cover-image");
-              image.src = url
-          }
-      });
-  })
+    firebase.auth().onAuthStateChanged((user) => {
+        let ref = firebase.storage().ref('users/coverPicture')
+        ref.child(user.uid).getDownloadURL().then(url => {
+            if (url) {
+                let image = document.getElementById("cover-image");
+                image.src = url
+            }
+        });
+    })
 }
