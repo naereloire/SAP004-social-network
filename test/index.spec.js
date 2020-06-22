28d2c01ba59377060c0466d82618e481a9946877
naereloire <naere.loire@gmail.com>
2020-06-22 01:28:20 -0300

    remove data duplicado

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,4 @@
-import { greeting } from '../public/pages/home/data.js';
+// import { createPost,loadPosts,saveImage,deletePost,savePostEdit,saveLike } from '../public/pages/home/data.js';
 
 describe('Greeting', () => {
   it('Deveria retornar "Oi Maria! Que bom ver você aqui!" quando passado "Maria" como parâmetros', () => {
